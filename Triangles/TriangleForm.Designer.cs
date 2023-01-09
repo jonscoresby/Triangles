e460b90bfdbcf5584e31d64179ee2f4480a75d4c
Jonathan Scoresby <me@jonscoresby.com>
2023-01-09 16:27:16 -0700

    Updated form name

--- Triangles/TriangleForm.Designer.cs
+++ Triangles/TriangleForm.Designer.cs
@@ -146,7 +146,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "TriangleForm";
-            this.Text = "Form1";
+            this.Text = "Triangles";
             this.ResumeLayout(false);
             this.PerformLayout();
 
