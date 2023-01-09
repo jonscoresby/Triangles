4835145360c1f201f0297448b5c0d6db46cdb1df
Jonathan Scoresby <me@jonscoresby.com>
2023-01-09 13:07:16 -0700

    Design form

--- Triangles/TriangleForm.cs
+++ Triangles/TriangleForm.cs
@@ -1,15 +1,15 @@
 namespace Triangles
 {
-    public partial class Form1 : Form
+    public partial class TriangleForm : Form
     {
-        public Form1()
+        public TriangleForm()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
